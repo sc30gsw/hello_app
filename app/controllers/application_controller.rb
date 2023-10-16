ad6cf169c262d49bd4184b83af2e27299c4c5261
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-16 16:10:37 +0900

    1.3.4演習1の解答

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world!"
+    # 1.3.4演習1
+    render html: "hola, mundo!"
   end
 end
